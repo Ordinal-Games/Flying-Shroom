066cb4193ddc4ff488aad99f9c39bcade0e5a3a6
TriangleDevv <chandu.lanka13@gmail.com>
2021-09-13 18:21:37 +0530

    🛒Got SDL2 working

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+
+int main(int argc, char* argv[]) {
+    std::cout << "SDL Works" << std::endl;
+    std::cin.get();
+
+    return 0;
+}
